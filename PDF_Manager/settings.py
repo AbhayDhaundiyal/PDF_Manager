acd72730677b8b582c4912c0159bc85718dd9ea3
AbhayDhaundiyal <abhaydhaundiyalktd67@gmail.com>
2023-07-11 21:51:51 +0530

    resolving cors error

--- PDF_Manager/settings.py
+++ PDF_Manager/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = False
 
 ROOT_URLCONF = 'PDF_Manager.urls'
 
