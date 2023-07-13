93bb6f6aded641994b3308cd493077a5983424f4
AbhayDhaundiyal <abhaydhaundiyalktd67@gmail.com>
2023-07-14 00:39:35 +0530

    changed the db structure

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import datetime
 
 class File(models.Model):
     file_id = models.BigIntegerField(null= False, default= -1)
@@ -10,6 +10,8 @@ class FileDetails(models.Model):
     file_id = models.AutoField(primary_key= True)
     file_name = models.CharField(max_length= 50)
     is_public = models.BooleanField(default= False)
+    owner = models.IntegerField(default= 1)
+    created_at = models.DateTimeField(default = datetime.now())
 
 
 class FileShared(models.Model):
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
+import datetime
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from IAM.models import User
 from dashboard.models import FileShared, File, FileDetails
 from IAM.utils.utils import verify_token
 from dashboard.serializers import FileDetailsSerializer
@@ -34,6 +36,8 @@ class PDFView(APIView):
             payload = request.data
             file_details = FileDetails()
             file_details.file_name = payload["fileName"]
+            file_details.owner = header_payload["user_id"]
+            file_details.created_at = datetime.datetime.now()
             file_details.save()
             file = File()
             file.file_id = file_details.file_id
@@ -51,7 +55,7 @@ class PDFView(APIView):
             return response
         except Exception as e:
             response = HttpResponse(str(e))
-            response.status_code = 400
+            response.status_code = 500
             return response
         
 
@@ -68,5 +72,36 @@ class OpenPDFView(APIView):
             return response
         except Exception as e:
             response = HttpResponse(str(e))
-            response.status_code = 200
+            response.status_code = 500
             return response
+    def patch(self, request):
+        try:
+            payload = verify_token(request.headers["Authorization"].split(" ")[1])
+            data = request.data
+            file_detail = FileDetails.objects.filter(file_id = data["file_id"], owner = payload["user_id"])
+            if len(file_detail) == 0:
+                raise Exception("Permission Denied")
+            if data["operation"] == "share":
+                user = User.objects.filter(id = data["second_user"])
+                if len(user) == 0:
+                    raise Exception("No such user")
+                file_shared = FileShared()
+                file_shared.user_id = data["second_user"]
+                file_shared.file_id = data["file_id"]
+                file_shared.save()
+            elif data["operation"] == "make_public":
+                file_detail = FileDetails.objects.get(file_id = data["file_id"])
+                file_detail.is_public = not file_detail.is_public
+                file_detail.save()
+                if file_detail.is_public:
+                    response = JsonResponse({"file_status" : file_detail.is_public, "message" : "made the file public"})
+                else:
+                    response = JsonResponse({"file_status" : file_detail.is_public, "message" : "made the file available to selected users"})
+                response.status_code = 200
+                return response
+        except Exception as e:
+            response = HttpResponse(str(e))
+            response.status_code = 400
+            return response
+        
+        
