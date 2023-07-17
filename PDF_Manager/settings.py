3a8b16c03b9f23fc6d010e2340dcab1b24624b33
AbhayDhaundiyal <abhaydhaundiyalktd67@gmail.com>
2023-07-17 12:53:26 +0530

    removed unused password hashers

--- PDF_Manager/settings.py
+++ PDF_Manager/settings.py
@@ -47,10 +47,6 @@ INSTALLED_APPS = [
 
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.Argon2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
-    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
-    "django.contrib.auth.hashers.ScryptPasswordHasher",
 ]
 
 
